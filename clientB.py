2b6fa545d26a02d38146da5feba3c5c6093bc1e2
sampsonliao <sliao@csumb.edu>
2019-03-17 13:06:00 -0700

    changed prompt

--- clientB.py
+++ clientB.py
@@ -7,7 +7,7 @@ serverName = 'localhost'
 serverPortB = 13000
 clientSocketB = socket(AF_INET, SOCK_STREAM)
 clientSocketB.connect((serverName,serverPortB))
-message = input('Enter your name:')
+message = input('Enter name of client followed by your name:')
 clientSocketB.send(message.encode())
 serverAck = clientSocketB.recv(1024)
 print ('From Server:', serverAck.decode())
